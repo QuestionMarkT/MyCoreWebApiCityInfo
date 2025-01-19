5e686a2d9f374791e00d064d50011b42d0c1382b
? <LP3CG@HOTMAIL.COM>
2025-01-19 02:54:37 +0100

    File controller added

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCoreWebApiCityInfo.Controllers;
+
+[Route("api/[controller]"), ApiController]
+public class FilesController : ControllerBase
+{
+    [HttpGet("{fileId}")]
+    public ActionResult GetFile(string fileId)
+    {
+        FileContentResult rp;
+        FileStreamResult result;
+        PhysicalFileResult physicalFileResult;
+        VirtualFileResult virtualFileResult = new("","");
+        return null;
+    }
+}
